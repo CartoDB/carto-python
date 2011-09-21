b58451dabab3e1ad8da0ffa9732c201385babe6e
javi <qualopec@gmail.com>
2011-09-21 19:06:28 +0200

    updated to new auth version

--- cartodb/cartodb.py
+++ cartodb/cartodb.py
@@ -2,88 +2,95 @@
 
 """
   ** CartoDBClient **
- 
+
     A simple CartoDB client to perform requests against the CartoDB API.
     Internally it uses OAuth
 
   * Requirements:
 
-     python 2.5 
-     pip install oauth
+     python 2.5
+     pip install oauth2
+     pip install simplejson # if you're running python < 2.6
 
   * Example use:
-     from cartodb import CartoDB
-     db = CartoDB(CONSUMER_KEY, CONSUMER_SECRET)
-     print db.run_sql("select * from test")
+        user =  'your@mail.com'
+        password =  'XXXX'
+        CONSUMER_KEY='XXXXXXXXXXXXXXXXXX'
+        CONSUMER_SECRET='YYYYYYYYYYYYYYYYYYYYYYYYYY'
+        cartodb_domain = 'vitorino'
+        cl = CartoDB(CONSUMER_KEY, CONSUMER_SECRET, user, password, cartodb_domain)
+        print cl.sql('select * from a')
 
 """
 
-import httplib
-import time
-import oauth.oauth as oauth
-import urlparse, cgi
+import urlparse
+import oauth2 as oauth
 import urllib
-import json
+import httplib2
+import sys
 
-from simpleoauth import SimpleOAuthClient
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
-SERVER = 'api.cartodb.com'
-PORT = 443
+from oauth2 import Request
 
-REQUEST_TOKEN_URL = 'https://api.cartodb.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.cartodb.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.cartodb.com/oauth/authorize'
-CALLBACK_URL = 'http://foobar.com'
-RESOURCE_URL = 'https://api.cartodb.com/v1' 
+REQUEST_TOKEN_URL = 'https://%(user)s.cartodb.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://%(user)s.cartodb.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://%(user)s.cartodb.com/oauth/authorize'
+RESOURCE_URL = 'https://%(user)s.cartodb.com/api/v1/sql'
 
 
 class CartoDB(object):
-    """ base client to access cartodb api """
+    """ basic client to access cartodb api """
 
-    def __init__(self, key, secret):
-        self.token = self.get_access_token(key, secret)
+    def __init__(self, key, secret, email, password, cartodb_domain):
 
-    def get_access_token(self, key, secret):
-        self.client = client = SimpleOAuthClient(SERVER, PORT, REQUEST_TOKEN_URL, ACCESS_TOKEN_URL, AUTHORIZATION_URL)
-        consumer = self.consumer = oauth.OAuthConsumer(key, secret)
-        signature_method_plaintext = oauth.OAuthSignatureMethod_PLAINTEXT()
-        self.signature_method_hmac_sha1 = oauth.OAuthSignatureMethod_HMAC_SHA1()
+        self.consumer_key = key
+        self.consumer_secret = secret
+        consumer = oauth.Consumer(self.consumer_key, self.consumer_secret)
 
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, callback=CALLBACK_URL, http_url=client.request_token_url)
-        oauth_request.sign_request(self.signature_method_hmac_sha1, consumer, None)
-        token = client.fetch_request_token(oauth_request)
-        oauth_request = oauth.OAuthRequest.from_token_and_callback(token=token, http_url=client.authorization_url)
-        # this will actually occur only on some callback
-        response = client.authorize_token(oauth_request)
-        # sad way to get the verifier
-        query = urlparse.urlparse(response)[4]
-        params = cgi.parse_qs(query, keep_blank_values=False)
-        verifier = params['oauth_verifier'][0]
+        client = oauth.Client(consumer)
+        client.set_signature_method = oauth.SignatureMethod_HMAC_SHA1()
 
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, token=token, verifier=verifier, http_url=client.access_token_url)
-        oauth_request.sign_request(self.signature_method_hmac_sha1, consumer, token)
-        token = client.fetch_access_token(oauth_request)
-        return token
+        params = {}
+        params["x_auth_username"] = email
+        params["x_auth_password"] = password
+        params["x_auth_mode"] = 'client_auth'
 
-    def get_resource(self, resource, parameters=None):
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(self.consumer, token=self.token, http_method='GET', http_url=resource, parameters=parameters)
-        oauth_request.sign_request(self.signature_method_hmac_sha1, self.consumer, self.token)
-        params = self.client.access_resource(oauth_request, resource)
-        return params
+        # Get Access Token
+        access_token_url = ACCESS_TOKEN_URL % {'user': cartodb_domain}
+        resp, token = client.request(access_token_url, method="POST", body=urllib.urlencode(params))
+        access_token = dict(urlparse.parse_qsl(token))
+        token = oauth.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
 
-    def run_sql(self, sql, parse_json=True):
-        """ executes sql query on cartodb server 
-            if parse_json is False, returned data isn't parsed as json
-            and is returned as is
+        # prepare client
+        self.resource_url = RESOURCE_URL % {'user': cartodb_domain}
+        self.client = oauth.Client(consumer, token)
+
+
+    def req(self, url, http_method="GET", http_headers=None):
+        """ make an autorized request """
+        resp, content = self.client.request(
+            url,
+            method=http_method,
+            headers=http_headers
+        )
+        return resp, content
+
+    def sql(self, sql, parse_json=True):
+        """ executes sql in cartodb server
+            set parse_json to False if you want raw reponse
         """
-        p = urllib.urlencode({'sql': sql})
-        #TODO: parse json and return python objects
-        data = self.get_resource(RESOURCE_URL + '?' + p)
-        if parse_json:
-            return json.loads(data)
-        return data
+        p = urllib.urlencode({'q': sql})
+        url = self.resource_url + '?' + p
+        resp, content = self.req(url);
+        if resp['status'] == '200':
+            if parse_json:
+                return json.loads(content)
+            return content
+        return None
 
-if __name__ == '__main__':
-    db = CartoDB(CONSUMER_KEY, CONSUMER_SECRET)
-    print db.run_sql("select * from test")
-    print db.run_sql("select * from test", False)
+
+
