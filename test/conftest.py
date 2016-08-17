ee988e357b021538373494dec49b2e325e5e4889
Dani Carrion <daniel@cartodb.com>
2016-08-17 08:02:59 +0200

    comment user tests

--- test/conftest.py
+++ test/conftest.py
@@ -8,14 +8,26 @@ from carto.models import UserManager
 
 @pytest.fixture(scope="session")
 def api_key_auth_client():
+    """
+    Returns a API key authentication client that can be used to send authenticated test requests to CARTO
+    :return: APIKeyAuthClient instance
+    """
     return APIKeyAuthClient(API_KEY, USERNAME, ORGANIZATION)
 
 
 @pytest.fixture(scope="session")
 def no_auth_client():
+    """
+    Returns an authentication client that can be used to send anonymous test requests to CARTO
+    :return: NoAuthClient instance
+    """
     return NoAuthClient()
 
 
 @pytest.fixture(scope="session")
 def user():
+    """
+    Handy way for tests to have access to a user object
+    :return: User instance that correspond to the test user
+    """
     return UserManager(APIKeyAuthClient(API_KEY, USERNAME, ORGANIZATION)).get(USERNAME) if ORGANIZATION is not None else None
--- test/test_users.py
+++ test/test_users.py
@@ -8,19 +8,37 @@ from carto.models import UserManager
 
 @pytest.fixture(scope="module")
 def user_manager(api_key_auth_client):
+    """
+    Returns a user manager that can be reused in tests
+    :param api_key_auth_client: Fixture that provides a valid APIKeyAuthClient object
+    :return: UserManager instance
+    """
     return UserManager(api_key_auth_client)
 
 
 def test_get_users(user_manager):
+    """
+    Currently not supported by the user API. If we actually tried to perform this test, an exception would be raised
+    :param user_manager: User manager to work with
+    """
     pass
 
 
-def test_get_one_user(user_manager, user):
+def test_get_one_user(user):
+    """
+    Test retrieval of a single user from the API
+    :param user: There is a fixture that returns a user object, so let's use it instead of specifically requesting a user
+    """
     assert user is not None
     assert user.username == USERNAME
 
 
 def test_modify_user(user_manager, user):
+    """
+    Test modifying a user
+    :param user_manager: User manager to work with
+    :param user: User to be modified
+    """
     old_email = user.email
     new_email = "a" + old_email
 
@@ -30,6 +48,7 @@ def test_modify_user(user_manager, user):
     user = user_manager.get(USERNAME)
     assert user.email == new_email
 
+    # Let's undo the change
     user.email = old_email
     user.save()
 
@@ -38,6 +57,10 @@ def test_modify_user(user_manager, user):
 
 
 def test_create_and_delete_user(user_manager):
+    """
+    Test creating a user and then deleting it
+    :param user_manager: User manager to work with
+    """
     new_user = user_manager.create(username="test-carto-python-sdk-1r2t3", password="test_8g7d6", email="test_carto_python_sdk_1r2t3@test.com")
     # API_ISSUE: following assert won't pass until https://github.com/CartoDB/cartodb/issues/9455 is fixed
     # assert new_user._id is not None
