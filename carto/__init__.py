0248586cf5542241dcb5f70bbb9d7640706d57f9
Dani Carrion <daniel@cartodb.com>
2016-08-18 20:05:56 +0200

    emptied main __init__.py

--- carto/__init__.py
+++
@@ -1,7 +0,0 @@
-from .models import *
-#from .auth import BaseAuthClient, APIKeyAuthClient, NoAuthClient
-#from .import_api import FileImportJob, SyncTableJob, FileImportManager, SyncTableImportManager, ExportJob
-#from .sql_api import SQLClient, BatchSQLClient, BatchSQLManager
-#from .core import CartoException
-#from .maps_api import NamedMap, NamedMapManager
-#__all__ = ['submodule1', 'submodule2']
