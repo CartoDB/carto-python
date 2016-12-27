8da91964e08483890c4fd0c5c2c3927e8af9b999
oriolbx <oriolbx@gmail.com>
2016-12-27 17:44:28 +0100

    update map example

--- examples/mapInfo.py
+++ examples/mapInfo.py
@@ -8,12 +8,12 @@ import pprint
 printer = pprint.PrettyPrinter(indent=4)
 from carto.sql import SQLClient
 from carto.visualizations import VisualizationManager
-
+import sys
 
 
 organization = 'cartoworkshops'
 CARTO_BASE_URL='https://carto-workshops.carto.com/api/'
-CARTO_API_KEY = 'dba84d3096d19b477cd2656da3edf616672ab530'
+CARTO_API_KEY = os.environ['CARTO_API_KEY']
 
 # work with CARTO entities. DatasetManager encapsulates information of a table
 auth_client = APIKeyAuthClient(CARTO_BASE_URL, CARTO_API_KEY, organization)
@@ -23,7 +23,10 @@ visualization_manager = VisualizationManager(auth_client)
 
 all_maps = visualization_manager.all()
 
-#map_user = visualization_manager.get("sf_trees")
-#printer.pprint(map_user.__dict__)
+mapa = visualization_manager.get(sys.argv[1])
+printer.pprint(mapa.__dict__)
 
-printer.pprint(all_maps.table.__dict__)
+
+
+
+
