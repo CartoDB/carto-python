34aceaf3b55e98b20ef3e041868b9ff51e174f35
oriolbx <oriolbx@gmail.com>
2017-02-15 18:10:18 +0100

    fixes issue #39

--- examples/map_info.py
+++ examples/map_info.py
@@ -8,6 +8,7 @@ import pprint
 printer = pprint.PrettyPrinter(indent=4)
 from carto.sql import SQLClient
 from carto.visualizations import VisualizationManager
+import urllib
 
 # Logger (better than print)
 import logging
@@ -25,7 +26,10 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--map', type=str, dest='map_name',
                     default=None,
-                    help='Set the name of the map to explore')
+                    help='Set the name of the map to explore and display its information on the console')
+parser.add_argument('--export', type=str, dest='export_map',
+                    default=None,
+                    help='Set the name of the map and export it')
 parser.add_argument('--organization', type=str, dest='organization',
                     default=os.environ['CARTO_ORG'],
                     help='Set the name of the organization' +
@@ -47,15 +51,33 @@ args = parser.parse_args()
 # Set authentification to CARTO
 auth_client = APIKeyAuthClient(
     args.CARTO_BASE_URL, args.CARTO_API_KEY, args.organization)
+
 visualization_manager = VisualizationManager(auth_client)
 
 # Render map info or the name of the maps
-if args.map_name == None:
+if args.map_name == None and args.export_map == None:
     for a_map in visualization_manager.all():
         logging.info(a_map.name)
-else:
+elif args.map_name != None and args.export_map == None:
     mapa = visualization_manager.get(args.map_name)
-    a = mapa.export()
-    logger.info('URL to download the map is: ' + a)
     printer.pprint(mapa.__dict__)
     printer.pprint(mapa.table.__dict__)
+elif args.map_name == None and args.export_map != None:
+    mapa_exp = visualization_manager.get(args.export_map)
+    current_path = os.getcwd()
+    logger.info('Map will be downloaded at {}'.format(current_path))
+    f = open(current_path+'/downloaded_file.carto','wb')
+    f.write(urllib.urlopen(mapa_exp.export()).read())
+    f.close()
+    logger.info('Map downloaded')
+else:
+    mapa = visualization_manager.get(args.map_name)
+    printer.pprint(mapa.__dict__)
+    printer.pprint(mapa.table.__dict__)
+    mapa_exp = visualization_manager.get(args.export_map)
+    current_path = os.getcwd()
+    logger.info('Map will be downloaded at {}'.format(current_path))
+    f = open(current_path+'/downloaded_file.carto','wb')
+    f.write(urllib.urlopen(mapa_exp.export()).read())
+    f.close()
+    logger.info('Map downloaded')
