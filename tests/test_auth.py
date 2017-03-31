8068ba570d72d62cdcef1b6f416f34e0e612ecd8
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-03-31 10:02:49 -0400

    removes unneeded test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,14 +12,6 @@ def test_cloud_personal_url():
     assert user2 == 'user2'
     assert user3 == 'user3'
 
-def test_cloud_organization_url():
-    user1 = APIKeyAuthClient('https://carto.com/u/user1/a/b/c', API_KEY).username
-    user2 = APIKeyAuthClient('https://www.carto.com/u/user2', API_KEY).username
-    user3 = APIKeyAuthClient('http://www.carto.com/u/user3/a/b/c', API_KEY).username
-    assert user1 == 'user1'
-    assert user2 == 'user2'
-    assert user3 == 'user3'
-
 def test_on_prem_url():
     user1 = APIKeyAuthClient('https://carto.com/user/user1/a/b/c', API_KEY).username
     user2 = APIKeyAuthClient('https://www.carto.com/user/user2', API_KEY).username
