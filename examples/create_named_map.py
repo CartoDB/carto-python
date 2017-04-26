9c0d9fc6ed5134ed139989f2615a32ce5848a48f
Alberto Romeu <alrocar@cartodb.com>
2017-04-26 10:19:15 +0200

    Example for creating a named map

---
+++ examples/create_named_map.py
@@ -0,0 +1,64 @@
+from carto.auth import APIKeyAuthClient
+import warnings
+warnings.filterwarnings('ignore')
+import os
+from carto.maps import NamedMapManager, NamedMap
+import logging
+import re
+import json
+
+# python create_named_map.py files/named_map.json
+
+# Logger (better than print)
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format=' %(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%I:%M:%S %p')
+logger = logging.getLogger()
+
+# set input arguments
+import argparse
+parser = argparse.ArgumentParser(
+    description='Creates a named map')
+
+parser.add_argument('named_map_json', type=str,
+                    help='Path to the named map JSON description file')
+
+parser.add_argument('--organization', type=str, dest='organization',
+                    default=os.environ['CARTO_ORG'],
+                    help='Set the name of the organization' +
+                    ' account (defaults to env variable CARTO_ORG)')
+
+parser.add_argument('--base_url', type=str, dest='CARTO_BASE_URL',
+                    default=os.environ['CARTO_API_URL'],
+                    help='Set the base URL. For example:' +
+                    ' https://username.carto.com/ ' +
+                    '(defaults to env variable CARTO_API_URL)')
+
+parser.add_argument('--api_key', dest='CARTO_API_KEY',
+                    default=os.environ['CARTO_API_KEY'],
+                    help='Api key of the account' +
+                    ' (defaults to env variable CARTO_API_KEY)')
+
+args = parser.parse_args()
+
+# Set authentification to CARTO
+auth_client = APIKeyAuthClient(
+    args.CARTO_BASE_URL, args.CARTO_API_KEY, args.organization)
+
+# get username from base_url
+substring = re.search('https://(.+?).carto.com', args.CARTO_BASE_URL)
+if substring:
+    username = substring.group(1)
+
+named_map_manager = NamedMapManager(auth_client)
+n = NamedMap(named_map_manager.client)
+
+with open(args.named_map_json) as named_map_json:
+    template = json.load(named_map_json)
+
+# Create named map
+named = named_map_manager.create(template=template)
+
+print('Named map created with ID: ' + named.template_id)
