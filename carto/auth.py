e1a8a0edafcafa77dfbbeddf438c8d3fa1a38bfc
Mario de Frutos <ethervoid@gmail.com>
2018-10-17 18:16:45 +0200

    Relative path passed using join fails in pyrestcli if it has / at the beggining
    pyrestcli uses urljoin to merge base_url and the relative_path but if
    the relative_path has / at the beggining, it overrides the current path
    in the base url and that's a problem if the base url is like
    https://carto.com/user/testuser so for example:
    
    base url: https://carto.com/user/testuser
    send path: /api/v3/endpoint
    
    the url used in the send method is going to be:
    https://carto.com/api/v3/endpoint removing the /user/carto path which is
    really important for us

--- carto/auth.py
+++ carto/auth.py
@@ -248,7 +248,7 @@ class AuthAPIClient(_UsernameGetter, _BaseUrlChecker, BasicAuthClient):
 
         :return: True if the API key is considered valid for current user.
         """
-        res = self.send('/api/v3/api_keys', 'get')
+        res = self.send('api/v3/api_keys', 'get')
         return \
             res.ok and \
             self.api_key in (ak['token'] for ak in res.json()['result'])
