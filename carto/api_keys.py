6710c4b1b9fb03de11f8f3b47b0f6a3f6c27bdb7
Alberto Romeu <alrocar@cartodb.com>
2019-06-07 12:41:12 +0200

    auth API first commit

---
+++ carto/api_keys.py
@@ -0,0 +1,68 @@
+"""
+Module for working with CARTO Auth API
+
+https://carto.com/developers/auth-api/
+
+.. module:: carto.api_keys
+   :platform: Unix, Windows
+   :synopsis: Module for working with CARTO Auth API
+
+.. moduleauthor:: Alberto Romeu <alrocar@carto.com>
+
+
+"""
+
+from pyrestcli.fields import CharField, DateTimeField, Field
+
+from .resources import Resource, Manager
+from .exceptions import CartoException
+from .paginators import CartoPaginator
+
+
+API_VERSION = "v3"
+API_ENDPOINT = "api/{api_version}/api_keys/"
+
+
+class APIKey(Resource):
+    """
+    Represents an API key in CARTO. API keys are used to grant permissions to
+    tables and maps. See the Auth API reference for more info: https://carto.com/developers/auth-api/
+
+    """
+    name = CharField()
+    token = CharField()
+    type = CharField()
+    created_at = DateTimeField()
+    updated_at = DateTimeField()
+    grants = Field()
+
+    class Meta:
+        collection_endpoint = API_ENDPOINT.format(api_version=API_VERSION)
+        name_field = "name"
+
+    def regenerate_token(self):
+        """
+        Regenerates the associated token
+        
+        :return:
+
+        :raise: CartoException
+        """
+        try:
+            endpoint = (self.Meta.collection_endpoint
+                        + "{name}/token/regenerate"). \
+                format(name=self.name)
+
+            self.send(endpoint, "POST")
+        except Exception as e:
+            raise CartoException(e)
+
+
+class APIKeyManager(Manager):
+    """
+    Manager for the APIKey class.
+
+    """
+    resource_class = APIKey
+    json_collection_attribute = "result"
+    paginator_class = CartoPaginator
---
+++ tests/test_api_keys.py
@@ -0,0 +1,127 @@
+import pytest
+from time import time
+
+from pyrestcli.exceptions import NotFoundException, UnprocessableEntityError
+
+from carto.api_keys import APIKeyManager
+
+
+@pytest.fixture(scope="module")
+def api_key_manager(api_key_auth_client_usr):
+    """
+    Returns an APIKeyManager instance that can be reused in tests
+    :param api_key_auth_client: Fixture that provides a valid APIKeyAuthClient
+                                object
+    :return: APIKeyManager instance
+    """
+    return APIKeyManager(api_key_auth_client_usr)
+
+
+@pytest.fixture(scope="module")
+def no_auth_client_fixture(no_auth_client):
+    return no_auth_client
+
+
+def test_get_api_key_not_found(api_key_manager):
+    with pytest.raises(NotFoundException):
+        api_key_manager.get('non-existent')
+
+
+def create_api_key(api_key_manager, grants, api_key_name=None):
+    if api_key_name is None:
+        api_key_name = '_'.join(str(time()).split('.'))
+    api_key_manager.create(name=api_key_name, grants=grants)
+    return api_key_name
+
+
+def test_create_regular_api_key(api_key_manager):
+    grants = [
+        {
+            "type": "apis",
+            "apis": ["sql", "maps"]
+        },
+        {
+            "type": "database",
+            "tables": []
+        }
+    ]
+    api_key_name = create_api_key(api_key_manager, grants)
+    api_key = api_key_manager.get(api_key_name)
+    assert api_key.name == api_key_name
+    assert api_key.type == 'regular'
+    assert api_key.grants == grants
+
+    api_key.delete()
+
+
+def test_regenerate_token(api_key_manager):
+    grants = [
+        {
+            "type": "apis",
+            "apis": ["sql", "maps"]
+        },
+        {
+            "type": "database",
+            "tables": []
+        }
+    ]
+    api_key_name = create_api_key(api_key_manager, grants)
+    api_key = api_key_manager.get(api_key_name)
+    old_token = api_key.token
+    api_key.regenerate_token()
+    assert old_token != api_key.token
+
+    api_key.delete()
+
+
+def test_create_invalid_api_key(api_key_manager):
+    with pytest.raises(UnprocessableEntityError):
+        api_key_name = 'wrong_grants'
+        api_key_manager.create(name=api_key_name, grants=[])
+
+
+def test_duplicate_key(api_key_manager):
+    grants = [
+        {
+            "type": "apis",
+            "apis": ["sql", "maps"]
+        },
+        {
+            "type": "database",
+            "tables": []
+        }
+    ]
+    api_key_name = create_api_key(api_key_manager, grants)
+    with pytest.raises(UnprocessableEntityError):
+        create_api_key(api_key_manager, grants, api_key_name)
+
+    api_key = api_key_manager.get(api_key_name)
+    api_key.delete()
+
+
+def test_api_key_manager_get_all(api_key_manager):
+    api_keys = api_key_manager.all()
+
+    assert len(api_keys) >= 2
+
+
+def test_api_key_manager_filter_master(api_key_manager):
+    api_keys = api_key_manager.filter(type='master')
+
+    assert len(api_keys) == 1
+    assert api_keys[0].type == 'master'
+    assert api_keys[0].token is not None
+
+
+def test_api_key_manager_filter_default(api_key_manager):
+    api_keys = api_key_manager.filter(type='default')
+
+    assert len(api_keys) == 1
+    assert api_keys[0].type == 'default'
+    assert api_keys[0].token is not None
+
+
+def test_api_key_manager_filter_multiple(api_key_manager):
+    api_keys = api_key_manager.filter(type='default,master')
+
+    assert len(api_keys) == 2    
