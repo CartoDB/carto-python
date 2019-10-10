4ca121d9c4e97719638e62de7e3ee1883628eae8
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-10 16:14:07 +0200

    Add do_subscriptions module

---
+++ carto/do_subscriptions.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Module for working with Data Observatory Subscriptions
+
+.. module:: carto.do_subscriptions
+   :platform: Unix, Windows
+   :synopsis: Module for working with Data Observatory Subscriptions
+
+.. moduleauthor:: Jesús Arroyo <jarroyo@carto.com>
+
+
+"""
+
+from pyrestcli.fields import CharField
+
+from .resources import Resource, Manager
+from .paginators import CartoPaginator
+
+
+API_VERSION = "v4"
+API_ENDPOINT = "api/{api_version}/do/subscriptions"
+
+
+class DOSubscription(Resource):
+    """
+    Represents a Data Observatory Subscriptions in CARTO.
+
+    """
+    dataset = CharField()
+    id = CharField()
+    project = CharField()
+    table = CharField()
+    type = CharField()
+
+    class Meta:
+        collection_endpoint = API_ENDPOINT.format(api_version=API_VERSION)
+        name_field = "id"
+
+
+class DOSubscriptionManager(Manager):
+    """
+    Manager for the DOSubscription class.
+
+    """
+    resource_class = DOSubscription
+    json_collection_attribute = "subscriptions"
+    paginator_class = CartoPaginator
